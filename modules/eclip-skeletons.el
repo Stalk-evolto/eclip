c614132fcec3949a1ecec18203093c6314e4ed05
stalk <stalk-evolto@outlook.com>
2026-04-28 19:06:25 +0800

    Add gplv3 skeletons.

--- modules/eclip-skeletons.el
+++ modules/eclip-skeletons.el
@@ -25,29 +25,30 @@
 ;;; Code:
 
 (require 'skeleton)
-(define-skeleton skel-scheme-header
-  "Insert Copyright to scheme file."
+(define-skeleton skel-gpl3
+  "Insert Copyright GPLv3 in file."
   "Short description: "
-  (comment-dwim 1) ?\s (file-name-nondirectory (buffer-file-name)) " --- " str
+  '(setopt comment-style 'extra-line)
+  @ ?\s (file-name-nondirectory (buffer-file-name)) " --- " str
   (make-string (max 2 (- 80 (current-column) 27)) ?\s)
-  ?\n (comment-dwim 1) " Copyright (C) " (format-time-string "%Y") "  "
+  ?\n "Copyright (C) " (format-time-string "%Y") "  "
   (getenv "ORGANIZATION") | (progn user-full-name)
   '(if (search-backward "&" (line-beginning-position) t)
        (replace-match (capitalize (user-login-name)) t t))
   '(end-of-line 1) " <" (progn user-mail-address) ">"
-  ?\n (comment-dwim 1)
-  ?\n (comment-dwim 1) " This program is free software; you can redistribute it and/or modify"
-  ?\n (comment-dwim 1) " it under the terms of the GNU General Public License as published by"
-  ?\n (comment-dwim 1) " the Free Software Foundation, either version 3 of the License, or"
-  ?\n (comment-dwim 1) " (at your option) any later version." ?\n
-  ?\n (comment-dwim 1) " This program is distributed in the hope that it will be useful,"
-  ?\n (comment-dwim 1) " but WITHOUT ANY WARRANTY; without even the implied warranty of"
-  ?\n (comment-dwim 1) " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-  ?\n (comment-dwim 1) " GNU General Public License for more details." ?\n
-  ?\n (comment-dwim 1) " You should have received a copy of the GNU General Public License"
-  ?\n (comment-dwim 1) " along with this program.  If not, see <https://www.gnu.org/licenses/>."
   ?\n
-  _
+  ?\n  "This program is free software; you can redistribute it and/or modify"
+  ?\n  "it under the terms of the GNU General Public License as published by"
+  ?\n  "the Free Software Foundation, either version 3 of the License, or"
+  ?\n  "(at your option) any later version." ?\n
+  ?\n  "This program is distributed in the hope that it will be useful,"
+  ?\n  "but WITHOUT ANY WARRANTY; without even the implied warranty of"
+  ?\n  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+  ?\n  "GNU General Public License for more details." ?\n
+  ?\n  "You should have received a copy of the GNU General Public License"
+  ?\n  "along with this program.  If not, see <https://www.gnu.org/licenses/>." @
+  ?\n '(comment-region (car (last skeleton-positions)) (car skeleton-positions))
+  ?\n _
   )
 
 (require 'tempo)
